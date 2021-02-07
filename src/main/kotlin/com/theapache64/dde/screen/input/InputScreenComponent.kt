2a74d071e719a2ca2eb4b58e3aeab53b897562d8
theapache64 <theapache64@gmail.com>
2021-02-07 11:25:51 +0530

    🔧 implement delegation on state read/write

--- src/main/kotlin/com/theapache64/dde/screen/input/InputScreenComponent.kt
+++ src/main/kotlin/com/theapache64/dde/screen/input/InputScreenComponent.kt
@@ -1,6 +1,8 @@
 package com.theapache64.dde.screen.input
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import com.arkivanov.decompose.ComponentContext
 import com.theapache64.dde.navigation.Component
@@ -10,20 +12,20 @@ class InputScreenComponent(
     private val onGoClicked: (name: String) -> Unit
 ) : Component, ComponentContext by componentContext {
 
-    private val state = mutableStateOf(State())
+    private var state by mutableStateOf(Model())
 
     @Composable
     override fun render() {
         InputScreen(
-            name = state.value.name,
+            name = state.name,
             onGoClicked = onGoClicked,
             onTextChanged = {
-                state.value = state.value.copy(name = it)
+                state = state.copy(name = it)
             }
         )
     }
 
-    private data class State(
+    private data class Model(
         val name: String = ""
     )
 }
